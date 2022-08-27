503d89ac9b327c49aaff584dbd9106956fda3852
100katty <ana.hernandez@hackademy.mx>
2022-08-27 15:30:10 -0500

    first commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+
+import { getGifs } from '../helpers/getGifs';
+
+
+export const useFetchGifs = (category) => {
+
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getImages = async () => {
+        const newImagenes = await getGifs(category);
+        setImages(newImagenes);
+        setIsLoading(false);
+    }
+   
+    
+    useEffect(() => {
+        getImages();
+    }, [])
+
+
+
+    return {
+        images ,
+        isLoading
+        
+    }
+}
